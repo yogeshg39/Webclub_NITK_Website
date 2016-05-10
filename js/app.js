6fd3b827a140812cad0481a657af9d608ba59e34
Yogesh Ghaturle <gyogesh0309@gmail.com>
2016-05-10 17:13:58 +0530

    Small robot working

---
+++ js/app.js
@@ -0,0 +1,46 @@
+function leftArrowPressed() {
+var element = document.getElementById("image1");
+element.style.left = parseInt(element.style.left) - 20 + 'px';
+element.style.transform = "rotate(-7deg)";
+}
+
+function rightArrowPressed() {
+var element = document.getElementById("image1");
+element.style.left = parseInt(element.style.left) + 20 + 'px';
+element.style.transform = "rotate(7deg)";
+}
+
+function upArrowPressed() {
+var element = document.getElementById("image1");
+element.style.top = parseInt(element.style.top) - 20 + 'px';
+}
+
+function downArrowPressed() {
+var element = document.getElementById("image1");
+element.style.top = parseInt(element.style.top) + 20 + 'px';
+}
+
+function moveSelection(evt) {
+    switch (evt.keyCode) {
+         case 37:
+          leftArrowPressed();
+          break;
+        case 39:
+            rightArrowPressed();
+            break;
+        case 38:
+            upArrowPressed();
+            break;
+        case 40:
+            downArrowPressed();
+            break;
+        }
+};
+
+function docReady()
+{
+          
+    window.addEventListener('keydown', moveSelection);
+
+  
+}
